d90947a0ba5215712838324dc48bd88ea148ef73
尚仁雪 <2375000960@qq.com>
2023-08-04 11:27:25 +0800

    1234

---
+++ src/components/aa.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+    <bb/>
+    </div>
+  </template>
+  
+  <script>
+  import bb from './bb.vue'
+  
+  export default {
+    name: 'aa',
+    components: {
+          bb
+    },
+    data(){
+      return {
+       
+      }
+    }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
+  
